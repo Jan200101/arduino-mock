cce34b04b40dbcbc5fa31b05a5fc3f96f412b519
Jan200101 <sentrycraft123@gmail.com>
2019-11-27 10:29:26 +0100

    correctly implement float printing

--- source/libarduino/src/serial/serial.cpp
+++ source/libarduino/src/serial/serial.cpp
@@ -1,12 +1,22 @@
 #define SERIAL_NODEFINE
 #include "serial/serial.hpp"
 #include <cstdio>
+#include <cstring>
 
-#define SERIAL_STR_PRINT(format, arg)           \
-    if (this->baud) return printf(format, arg); \
+constexpr char fltfmt[] = "%.";
+
+#define SERIAL_STR_PRINT(format, arg)                    \
+    if (this->baud) return fprintf(stderr, format, arg); \
     return 0
 
-#define SERIAL_PRINT SERIAL_STR_PRINT // TODO implement proper print for non str functions
+#define SERIAL_FLOAT_PRINT(decimal, val, newline) \
+    char str[sizeof(val) * 8 + 4];                \
+    snprintf(str, sizeof(val) * 8 + 4,            \
+             "%s%if", fltfmt, decimal);           \
+    if (newline) strcat(str, "\n");               \
+    SERIAL_PRINT(str, val);
+
+#define SERIAL_PRINT SERIAL_STR_PRINT
 
 C_Serial::C_Serial()
 {
@@ -25,7 +35,7 @@ size_t C_Serial::print(int val, BASEX) { SERIAL_PRINT("%i", val); }
 size_t C_Serial::print(unsigned int val, BASEX) { SERIAL_PRINT("%u", val); }
 size_t C_Serial::print(long val, BASEX) { SERIAL_PRINT("%li", val); }
 size_t C_Serial::print(unsigned long val, BASEX) { SERIAL_PRINT("%lu", val); }
-size_t C_Serial::print(double val, int) { SERIAL_PRINT("%.2f", val); }
+size_t C_Serial::print(double val, int decimal) { SERIAL_FLOAT_PRINT(decimal, val, false); }
 
 size_t C_Serial::println(const char* text) { SERIAL_STR_PRINT("%s\n", text); }
 size_t C_Serial::println(char text) { SERIAL_STR_PRINT("%c\n", text); }
@@ -34,5 +44,5 @@ size_t C_Serial::println(int val, BASEX) { SERIAL_PRINT("%i\n", val); }
 size_t C_Serial::println(unsigned int val, BASEX) { SERIAL_PRINT("%u\n", val); }
 size_t C_Serial::println(long val, BASEX) { SERIAL_PRINT("%li\n", val); }
 size_t C_Serial::println(unsigned long val, BASEX) { SERIAL_PRINT("%lu\n", val); }
-size_t C_Serial::println(double val, int) { SERIAL_PRINT("%.2f\n", val); }
-size_t C_Serial::println(void) { SERIAL_STR_PRINT("\n", ""); }
+size_t C_Serial::println(double val, int decimal) { SERIAL_FLOAT_PRINT(decimal, val, true); }
+size_t C_Serial::println(void) { SERIAL_STR_PRINT("%s", "\n"); }
